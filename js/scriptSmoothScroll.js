cb28ea689494e2a9315f67cbf7302fcdf04ad8f4
Unknown <marrcel12.marga@gmail.com>
2018-06-18 22:28:49 +0200

    Nowy smoothscroll

--- js/scriptSmoothScroll.js
+++ js/scriptSmoothScroll.js
@@ -1,23 +1,24 @@
-$(function(){
+$(function () {
     //zmiana koloru menu nav kiedy scrollujemy brakePoint 100px
-    $(window).scroll(function(){
-        if($(window).scrollTop()>100){
-         $(".navbar").css("background-color","white");
-         $(".nav-link").css("color", "black");   
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $(".navbar").css("background-color", "white");
+            $(".nav-link").css("color", "black");
+        } else {
+            $(".navbar").css("background-color", "");
+            $(".nav-link").css("color", "white");
         }
-        else {
-            $(".navbar").css("background-color","");
-            $(".nav-link").css("color", "white");  
-        }   
-        
-        
+
+
     });
 
-  //smooth scrolling 
-  $(document).on('click', '.Smooth', function(event){
-	    event.preventDefault();
-	    $('body').animate({
-	        scrollTop: $($.attr(this, 'href')).offset().top
-	    }, 800);
-	});
+    //smooth scrolling 
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
 });
